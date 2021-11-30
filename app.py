e67eb1a6c4db30728dd90f887d04a06033227cf1
rachelp4515 <rachel.porhammer@students.dominican.edu>
2021-11-30 16:05:43 -0500

    somewhat finished

--- app.py
+++ app.py
@@ -14,21 +14,18 @@ donos = db.donos
 #---------------------- / 
 @app.route('/')
 def dono_index():
-    """Show all donations."""
     return render_template('dono_index.html', donos=donos.find())
 
 
 #---------------------- / new dono
 @app.route('/donos/new')
 def new_dono():
-    """Add a donation"""
     return render_template('new_dono.html')
 
 
 #---------------------- / submit dono
 @app.route('/donos', methods=['POST'])
 def dono_submit():
-    """Submit donation"""
     dono = {
         'name': request.form.get('name'),
         'charity': request.form.get('charity'),
@@ -41,14 +38,12 @@ def dono_submit():
 #---------------------- / display one
 @app.route('/donos/<dono_id>')
 def donos_show(dono_id):
-    """Show a single donation"""
     dono = donos.find_one({'_id': ObjectId(dono_id)})
     return render_template('show_dono.html', dono=dono)
 
 #--------------------- / remove from display
 @app.route('/donos/<dono_id>/delete', methods=['POST'])
 def donos_delete(dono_id):
-    """Delete one donation."""
     donos.delete_one({'_id': ObjectId(dono_id)})
     return redirect(url_for('dono_index'))
 
@@ -56,39 +51,14 @@ def donos_delete(dono_id):
 
 @app.route('/donos/<dono_id>/add')
 def dono_edit(dono_id):
-    """Add to a donation"""
     dono = donos.find_one({'_id': ObjectId(dono_id)})
     return render_template('add_dono.html', dono=dono)
 
+#-------------------- / account
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# @app.route('/test', methods= ['POST'])
-# def test():
-#     dono = {
-#         'name': 'uhhhhhgggjff', 'amount':1, 'charity': 'asdf'
-#     }
-#     donos.insert_one(dono)
-#     return redirect("/")
-
-
-
-
-
+@app.route('/donos/account')
+def account_display():
+    return render_template('account.html')
 
 
 
